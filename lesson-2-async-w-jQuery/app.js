2645d9b92068104b658a85c15e433642d6f7a355
alex <alex_21@live.com>
2018-06-25 02:57:13 +0300

    feat: implement asynchronous requests via jquery

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -10,5 +10,21 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        $.ajax({
+            url: 'https://api.unsplash.com/search/photos?page=1&query=${searchedForText}',
+            headers:{
+                Authorization:'Client-ID 0bc6e7701a4fd1ad78ab4d31e402a87cc60f13425f74f22daac56de20db9d989'
+            }
+        }).done(addImage);
     });
+    function addImage(images) {
+        const firstImage = images.results[0];
+    
+        responseContainer.insertAdjacentHTML('afterbegin', `<figure>
+                <img src="${firstImage.urls.small}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`
+        );
+    }
 })();
